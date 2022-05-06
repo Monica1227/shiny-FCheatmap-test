99a1577543f30335b541830a0c0136c922e1646e
JingYang <58110162+Monica1227@users.noreply.github.com>
2022-05-06 18:17:34 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,66 @@
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("FCheatmap: a web-tool for generation of heatmaps among different comparison groups with significant labels",
+              tags$head(tags$style(type="text/css", "label.radio { display: inline-block; }", ".radio input[type=\"radio\"] { float: none; }"),
+                        tags$style(type="text/css", "select { max-width: 200px; }"),
+                        tags$style(type="text/css", "textarea { max-width: 185px; }"),
+                        tags$style(type="text/css", ".jslider { max-width: 200px; }"),
+                        tags$style(type='text/css', ".well { max-width: 330px; }"),
+                        tags$style(type='text/css', ".span4 { max-width: 330px; }")) 
+  ),
+  
+  # App title ----
+  titlePanel("Uploading Files"),
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select a file ----
+      tags$h1("Differential analysis results"),
+      fileInput("file1", "Choose the first File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+
+      fileInput("file2", "Choose the second File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+
+      fileInput("file3", "Choose the third File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+
+      fileInput("file4", "Choose the fourth File",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      # Horizontal line ----
+      tags$hr(),
+      submitButton("Submit", icon("sync"))
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      plotOutput('plot')
+    )
+  )
+)
+)
+      
+      
+   
+      
+      
+      
+
